7c609351f7e2f7e290b45b4efe4cb863db16c59c
eveligora <eveligora@gmail.com>
2021-08-31 14:49:30 +0300

    favicon move to helpers

--- wp-content/themes/dev-theme/header.php
+++ wp-content/themes/dev-theme/header.php
@@ -4,12 +4,6 @@
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>"/>
 	<meta name="description" content="<?php bloginfo( 'description' ) ?>"/>
-	<?php
-	$favicon = get_field( 'favicon', 'option' );
-	if ( $favicon ) : ?>
-		<link rel="icon" href="<?php echo esc_url( $favicon ) ?>" type="image/x-icon"/>
-		<link rel="shortcut icon" href="<?php echo esc_url( $favicon ) ?>" type="image/x-icon"/>
-	<?php endif; ?>
 
 	<?php wp_head(); ?>
 
