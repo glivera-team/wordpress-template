45445f6c10dd1bf223f1716c9456ab791065fb41
eveligora <eveligora@gmail.com>
2021-09-08 08:54:53 +0300

    adding template parts example + logo link conditions

--- wp-content/themes/dev-theme/templates/template_home.php
+++ wp-content/themes/dev-theme/templates/template_home.php
@@ -3,6 +3,7 @@
 get_header();
 ?>
 
-  <?php get_template_part('template_parts/universal/hero_block'); ?>
+  <?php get_template_part('template_parts/home_page/hero_block'); ?>
+  <?php get_template_part('template_parts/universal/contacts_block'); ?>
 
 <?php get_footer();
